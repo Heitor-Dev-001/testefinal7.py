e296b3ee30b61219a756fcccb6c9986ec69a0d0f
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-11-12 14:28:23 -0300

    Add files via upload

---
+++ testefinal007.py
@@ -0,0 +1,4 @@
+#Qual é o resultado calculado pelo Python para a expressão composta 3 * 5 + 4 ** 2?
+
+resultado = 3 * 5 + 4 **2
+print('O seu resulatdo é?:{}'.format(resultado)) #Resultado da conta
